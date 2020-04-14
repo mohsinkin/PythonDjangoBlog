d1a0edb69a871a933781ef90f5dd5125d2253da4
mossosaurus <mohsinkin@yahoo.com>
2020-04-13 19:21:18 -0700

    Formatting

--- vscode_env_test.py
+++ vscode_env_test.py
@@ -12,8 +12,8 @@ def greet(who_to_greet):
     greeting = "Hello, {}".format(who_to_greet)
     return greeting
 
+
 # print(greet('World'))
 # print(greet('Mohsin'))
 r = requests.get("https://google.com")
 print(r.status_code)
-
