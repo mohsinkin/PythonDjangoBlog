f814a4c3d85dff5be8264f3776dec286790a740b
mossosaurus <mohsinkin@yahoo.com>
2020-04-15 16:19:58 -0700

    Password Reset pages

--- reflections_django/reflections_django/urls.py
+++ reflections_django/reflections_django/urls.py
@@ -35,7 +35,23 @@ urlpatterns = [
         auth_views.LogoutView.as_view(template_name="users/logout.html"),
         name="logout",
     ),
-    path("", include("blog.urls")),
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"),
+        name="password_reset",
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset-confirm/uidb64/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html"),
+        name="password_reset_confirm",
+    ),
+    path("", include("blog.urls"))
 ]
 
 if settings.DEBUG:
