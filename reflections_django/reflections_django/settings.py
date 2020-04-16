0ba5f8484be7493de67658470b50c5a14c9f0bb2
mossosaurus <mohsinkin@yahoo.com>
2020-04-15 21:12:19 -0700

    django_heroku config

--- reflections_django/reflections_django/settings.py
+++ reflections_django/reflections_django/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,3 +124,5 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 LOGIN_REDIRECT_URL = "blog-home"
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
