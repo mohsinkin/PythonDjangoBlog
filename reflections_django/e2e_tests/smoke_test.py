c6527ff9401d7db8647336c2a9e34aae85e0ee7e
mossosaurus <mohsinkin@yahoo.com>
2020-04-30 00:07:31 -0700

    Adding IDs and updating test

--- reflections_django/e2e_tests/smoke_test.py
+++ reflections_django/e2e_tests/smoke_test.py
@@ -4,16 +4,21 @@ import time
 
 driver = webdriver.Chrome(executable_path=r'C:\Users\mohsi.MOSSOSAURUSPC\Downloads\chromedriver.exe')
 
-driver.get('https://moss-blog.herokuapp.com')
+driver.get('https://moss-blog.herokuapp.com/login')
 print(driver.title)
 
-driver.get('http://google.com')
-print(driver.title)
+user_name_elem = driver.find_element_by_name('username')
+password_elem = driver.find_element_by_name('password')
+submit_button = driver.find_element_by_css_selector('button.btn-outline-info')
+# print(user_name_elem.is_displayed())
+# print(user_name_elem.is_enabled())
+# print(password_elem.is_displayed())
+# print(password_elem.is_enabled())
 
-driver.back()
-print(driver.title)
+user_name_elem.send_keys('test_headless')
+password_elem.send_keys('Testing!@')
+submit_button.click()
 
-driver.forward()
-print(driver.title)
 
 driver.close()
+driver.quit()
